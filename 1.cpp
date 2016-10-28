8b7bd066fb7eae2af90adb826dfa3ccd644ab835
dodo325 <dodo101000101@gmail.com>
2016-10-28 22:13:02 +0300

    add NOK amd more...

--- 1.cpp
+++ 1.cpp
@@ -1,8 +1,8 @@
 #include "iostream"
 
 int binpow (int a, int n) {
-	if (n == 0)
-		return 1;
+	if (n < 0) throw -1;// Жуть)
+	if (n == 0) return 1;
 	if (n % 2 == 1)
 		return binpow (a, n-1) * a;
 	else {
@@ -10,18 +10,35 @@ int binpow (int a, int n) {
 		return b * b;
 	}
 }
-void rot(int n){
-  int n;
-vector<char> prime (n+1, true);
-prime[0] = prime[1] = false;
-for (int i=2; i<=n; ++i)
-	if (prime[i])
-		if (i * 1ll * i <= n)
-			for (int j=i*i; j<=n; j+=i)
-				prime[j] = false;
-
+double binpowP(int a, int n) {//
+	  if(n<0) return 1/double(binpow(a, -n));
+    else    return double(binpow(a,n));}
+void resch(int SZ){
+    int i, j;
+   //int SZ = 100;            // предел поиска простых чисел
+   for (i = 2; i < SZ; i++) //перебираем числа
+   {
+      bool y = 1;           // пусть будет простое
+      if (i%2 == 0) y = 0;  //делится нацело на 2, значит не простое
+      for (j = 3; (j*j <= i) && y; j+=2)
+            if (i%j == 0) {y = 0; break;} // делится нацело на какое
+                           //нибудь число до корня значит не простое
+      if(y) std::cout << i << std::endl;
+   }
+}
+int NOD(int a, int b){
+    while(a > 0 && b > 0)
+        if(a > b)
+            a %= b;
+        else
+            b %= a;
+    return a + b;
+}
+int NOK(int a, int b) {
+	return a*b/NOD(a,b);
 }
 int phi (int n) {
+	if(n <= 0 ) return -1; // 0 - это против правил)
 	int result = n;
 	for (int i=2; i*i<=n; ++i)
 		if (n % i == 0) {
@@ -33,11 +50,7 @@ int phi (int n) {
 		result -= result / n;
 	return result;
 }
-
-int main(int argc, char const *argv[]) {
-  int n = 0;
-  std::cin >> n;
-  std::cout << "phi( " << n << " )  = " << phi(n) << std::endl;
-
-  return 0;
+int main(){
+    std::cout<<"lol)";
+    return 0;
 }
